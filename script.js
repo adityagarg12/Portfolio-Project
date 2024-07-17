3025da0cc4c51e4d0dc8872f863fb013451179cf
adityagarg12 <adityagarg24052006@gmail.com>
2024-07-17 20:00:53 +0530

    STAGE-4 . theme switcher added

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function togglemenu(){
     icon.classList.toggle("open");
 
 }
+
+//switch function
+document.addEventListener('DOMContentLoaded', (event) => {
+    const switchTheme = () => {
+        const rootElem = document.documentElement;
+        let dataTheme = rootElem.getAttribute('data-theme');
+        let newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+
+        // Set new HTML attribute
+        rootElem.setAttribute('data-theme', newTheme);
+
+        // Set new local storage item
+        localStorage.setItem('theme', newTheme);
+    }
+
+    // Add event listener for switcher
+    const themeSwitcher = document.querySelector('.theme-switcher');
+    if (themeSwitcher) {
+        themeSwitcher.addEventListener('click', switchTheme);
+    } else {
+        console.error('Element with class "theme-switcher" not found.');
+    }
+});
